bfdc699542014e7d9d2e49aa95303d7120079b41
bogathi bhagavan reddy <155155137+bhagavan04@users.noreply.github.com>
2025-06-11 20:00:00 +0530

    Update chatbot.py

--- backend-python/app/api/routes/AIChatbot/chatbot.py
+++ backend-python/app/api/routes/AIChatbot/chatbot.py
@@ -4,7 +4,12 @@ from .helper import finance_chatbot
 router = APIRouter()
 
 @router.get("/chat")
-async def sentiment(query: str):
+# use Request when you want access (eg., for logging, auth) 
+async def sentiment(request:Request,query: str):
+    # you want cilent ip address
+    client_ip = request.client.host
+    print(f"Request from IP: {client_ip}")
+    return {"message": "OK"}
     try:
         result = finance_chatbot(query)
         return result
